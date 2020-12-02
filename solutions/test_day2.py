d7a0bd2ad5abeb3726df8a9ef213ce6548b5b31f
Jon Hoffman <jonhoffman@gmail.com>
2020-12-02 12:48:19 +0000

    day 2

---
+++ solutions/test_day2.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+from typing import List
+
+Password = namedtuple("Password", "min max letter password")
+
+
+def get_input() -> List[Password]:
+    passwords = []
+    with open("inputs/day2input.txt", "r") as file:
+        for l in file.readlines():
+            space_parts = l.split(" ")
+            range_parts = space_parts[0].split("-")
+            letter = space_parts[1][0]
+            password = space_parts[2].strip()
+            passwords.append(Password(int(range_parts[0]), int(range_parts[1]), letter, password))
+    return passwords
+
+
+def test_day2_part1():
+    assert len([p for p in get_input() if p.password.count(p.letter) in range(p.min, p.max + 1)]) == 517
+
+
+def test_day2_part2():
+    assert (
+        len([p for p in get_input() if (p.password[p.min - 1] == p.letter) != (p.password[p.max - 1] == p.letter)])
+        == 284
+    )
