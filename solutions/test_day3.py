80953731bb350a9cf44c7331e9a44238a07c6608
Jon Hoffman <jonhoffman@gmail.com>
2020-12-03 20:15:41 +0000

    inlined instructions

--- solutions/test_day3.py
+++ solutions/test_day3.py
@@ -1,19 +1,88 @@
+"""
+--- Day 3: Toboggan Trajectory ---
+With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+
+Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
+
+From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+
+The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........X.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...#X....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+
+--- Part Two ---
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+"""
+import operator
+from functools import reduce
 from typing import List
 
 
 def get_input() -> List[List[str]]:
     with open("inputs/day3input.txt", "r") as file:
-        return [list(line.strip()) for line in file.readlines()]
+        return [line.strip() for line in file.readlines()]
 
 
-def slope_counter(grid: List[List[str]], x_slope: int, y_slope: int) -> int:
+def slope_counter(grid: List[List[str]], dx: int, dy: int) -> int:
     x = 0
     y = 0
 
     tree_count = 0
     while y < len(grid) - 1:
-        x += x_slope
-        y += y_slope
+        x += dx
+        y += dy
         row = grid[y]
         cell_value = row[x % len(row)]
         if cell_value == "#":
@@ -28,11 +97,19 @@ def test_day3_part1():
 
 def test_day3_part2():
     grid = get_input()
-    tree_multiple = (
-        slope_counter(grid, 1, 1)
-        * slope_counter(grid, 3, 1)
-        * slope_counter(grid, 5, 1)
-        * slope_counter(grid, 7, 1)
-        * slope_counter(grid, 1, 2)
+
+    tree_multiple = reduce(
+        operator.mul,
+        map(
+            lambda p: slope_counter(grid, *p),
+            [
+                (1, 1),
+                (3, 1),
+                (5, 1),
+                (7, 1),
+                (1, 2),
+            ],
+        ),
+        1,
     )
     assert tree_multiple == 3154761400
